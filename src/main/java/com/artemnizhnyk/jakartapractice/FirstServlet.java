d6611f252a2985c77da1d8756d3c4db46abeca2b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-09 17:09:56 +0200

    First servlet added

---
+++ src/main/java/com/artemnizhnyk/jakartapractice/FirstServlet.java
@@ -0,0 +1,39 @@
+package com.artemnizhnyk.jakartapractice;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>Hi First Servlet</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroy");
+    }
+
+
+}
